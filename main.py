afd505d21079acf6887d99b12690fa4e7164a639
Rafael Alves <citygotham552@gmail.com>
2023-06-12 21:09:18 -0300

    Some files

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import pygame, sys
+
+class Ship(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(' graphics\ship.png').convert_alpha()
+        self.rect = self.image.get_rect(center = (DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2))
+
+# basic setup
 pygame.init()
 DISPLAY_WIDTH, DISPLAY_HEIGHT = 1280,720
-pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+display_surface = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 clock = pygame.time.Clock()
+
+# sprite groups
+spaceship_group = pygame.sprite.Group()
+
+# srite creatinon
+ship = Ship()
+spaceship_group.add(ship)
+
+# game loop
 while True:
+    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    clock.tick(120)
+
+    # delta time
+    dt = clock.tick(120) / 1000
+   
+    # Graphics
+    spaceship_group.draw(display_surface)
+
+    # draw the frame
     pygame.display.update()
